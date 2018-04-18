afe644281ace0dad75d1986cf43cab235de78dd3
Michael Eby <michaeleby1@gmail.com>
2018-04-18 02:48:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ function happyHolidays(string) {
 function happyHolidaysTo(name) {
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holdayCountdown(holiday, days) {
+  return `It's ${days} until ${holiday}!`
+}
